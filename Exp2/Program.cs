04ed3e0d23388b153c4b4aa0e244fce6f50b3b56
Nataly <tusikf@yandex.ru>
2023-06-13 20:00:50 +0300

    выполнила первую задачу

--- Exp2/Program.cs
+++ Exp2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+int max = number1;
+
+if (number2 > max) 
+{ max = number2; }
+
+Console.WriteLine($"Максимальное из этих чисел: { max }");
